d9c27249363f32b009bc2ea814bd0c72f6ae9d12
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-07 21:53:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 //your JS code here. If required.
 let input = document.querySelector("input")
+let colors = document.querySelectorAll("#colorSelect option")
+
 input.addEventListener("click",()=>{
-	let list = document.getElementById("colorSelect")
-	list.remove(list.selectedIndex)
+	let selected = document.querySelector("#colorSelect option:checked")
+	colors.forEach((color)=>{
+		if(color.value === selected.value){
+			color.remove()
+		}
+	})
 })
+
+
+
+
+
+
+
+// input.addEventListener("click",()=>{
+// 	let list = document.getElementById("colorSelect")
+// 	list.remove(list.selectedIndex)
+// })
